ae6702c6806f1341f884f010900a2ce66a5894ab
Rosa <rreynarive1@educacion.navarra.es>
2023-02-08 20:25:32 +0100

    ampliando

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,5 +1,4 @@
 import pygame
-
 #Se añade la barra nuestro videojuego
 pygame.init()
 ventana = pygame.display.set_mode((640,480))
@@ -12,8 +11,13 @@ ballrect.move_ip(0,0)
 
 bate = pygame.image.load("bate.png")
 baterect = bate.get_rect()
-
 baterect.move_ip(240,450)
+
+bricklist = []
+for brick in bricklist:
+    brick = pygame.image.load("ladrillo.png")
+    brickrect = brick.get_rect()
+
 
 jugando = True
 while jugando:
@@ -29,16 +33,22 @@ while jugando:
 
     if baterect.colliderect(ballrect):
         speed[1] = -speed[1]
+
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
         speed[1] = -speed[1]
+
+    if ballrect.colliderect(bricklist):
+        speed[1] = -speed[1]
+
+
     ventana.fill((252, 243, 207))
     ventana.blit(ball, ballrect)
-
-
     ventana.blit(bate, baterect)
+    ventana.blit(brick, brickrect)
+
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
