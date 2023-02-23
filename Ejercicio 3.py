3a108c5f94363cec96fa095657d747187d0927db
rreynarive <rreynarive1@educacion.navarra.es>
2023-02-23 19:51:54 +0100

    fondo añadido

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,49 +1,57 @@
 import pygame
-#Se añade la barra nuestro videojuego
+from random import randint
+
 pygame.init()
 ventana = pygame.display.set_mode((640,480))
-pygame.display.set_caption("Ejemplo 3")
+pygame.display.set_caption("Ejemplo 4")
+fondo = pygame.image.load("fondo.png")
+ventana.blit(fondo,(0,0))
 
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
-speed = [4,4]
-ballrect.move_ip(0,0)
+speed = [randint(3, 6), randint(3, 6)]
+ballrect.move_ip(0, 0)
 
-bate = pygame.image.load("bate.png")
-baterect = bate.get_rect()
-baterect.move_ip(240,450)
+barra = pygame.image.load("barra.png")
+barrarect = barra.get_rect()
+barrarect.move_ip(240, 450)
 
 bricklist = []
 for brick in bricklist:
-    brick = pygame.image.load("ladrillo.png")
+    brick = pygame.image.load("brick.png")
     brickrect = brick.get_rect()
 
+fuente = pygame.font.Font(None, 100)
 
 jugando = True
 while jugando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        baterect = baterect.move(-3,0)
+        barrarect = barrarect.move(-3, 0)
     if keys[pygame.K_RIGHT]:
-        baterect = baterect.move(3,0)
+        barrarect = barrarect.move(3, 0)
 
-    if baterect.colliderect(ballrect):
+    if barrarect.colliderect(ballrect):
         speed[1] = -speed[1]
 
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+    if ballrect.top < 0:
         speed[1] = -speed[1]
-
-
-    ventana.fill((252, 243, 207))
-    ventana.blit(ball, ballrect)
-    ventana.blit(bate, baterect)
+    if ballrect.bottom > ventana.get_height():
+        texto = fuente.render("Game Over", True, (125,125,125))
+        texto_rect = texto.get_rect()
+        texto_x = ventana.get_width() / 2 - texto_rect.width / 2
+        texto_y = ventana.get_height() / 2 - texto_rect.height / 2
+        ventana.blit(texto, [texto_x, texto_y])
+    else:
+        ventana.fill((252, 243, 207))
+        ventana.blit(ball, ballrect)
+        ventana.blit(barra, barrarect)
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
